d23afce8363ca5e060cfbdfe07fea449198df6b7
Yihui Xiong <yihui.xiong@hotmail.com>
2019-09-23 15:38:03 +0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,17 +4,15 @@
 SRC=$(dirname "$BASH_SOURCE")
 cd $SRC
 
-chmod +x hey_wifi hey_wifi.py io_service.py
+chmod a+x hey_wifi hey_wifi.py io_service.py play amp
 
 mkdir -p /usr/local/bin
-cp hey_wifi /usr/local/bin
-cp hey_wifi.py /usr/local/bin
-cp io_service.py /usr/local/bin
+cp hey_wifi hey_wifi.py io_service.py play amp /usr/local/bin
+
 
 mkdir -p /usr/local/share/quiet/
 cp quiet-profiles.json /usr/local/share/quiet/
 
-cp io.service /etc/systemd/system
-cp hey_wifi.service /etc/systemd/system
+cp io.service hey_wifi.service /etc/systemd/system
 
 systemctl enable io
